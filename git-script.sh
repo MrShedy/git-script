e4820b76bb9343711c1fca57a69a9af23d1da8b9
Mr_Shedy <56317987+MrShedy@users.noreply.github.com>
2020-03-06 23:17:48 +0300

    Show author at start

--- git-script.sh
+++ git-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Program made by MrShedy"
+
 rd=$1
 git init $rd #2
 cd $rd #3
@@ -7,4 +9,3 @@ echo "* text=auto" > .gitattributes #4
 
 git add .
 git commit -m "Initinal setup"
-
